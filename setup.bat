b9cdfa1c9c0278ee5a3a45df7ee42f49a4f03a9a
MiniJ147 <47308554+MiniJ147@users.noreply.github.com>
2023-12-08 03:29:51 -0500

    added setup tool to install all dependencies

---
+++ setup.bat
@@ -0,0 +1,4 @@
+@echo off 
+::must have node and npm installed for this to work
+@echo "attemping setup | may have to npm init before hand"
+npm install express body-parser ejs mongodb dotenv multer
